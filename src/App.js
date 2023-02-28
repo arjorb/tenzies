4dd0169e7f0dbc8d81f68e8571163244dbc29dc7
John U <arjorb@gmail.com>
2023-02-28 15:54:33 +0200

    done seting up the environment

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='App'>
+      <h1 className='text-3xl font-bold text-[#049c88] underline'>Seting up the environment</h1>
+    </div>
+  );
+}
+
+export default App;
