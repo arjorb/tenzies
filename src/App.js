73f4a3ffba9e74ebd14dbfefc8e9d391a1b09031
John U <arjorb@gmail.com>
2023-02-28 18:11:28 +0200

    done working on the rectangles both black and gray

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div className='App'>
-      <h1 className='text-3xl font-bold text-[#049c88] underline'>Seting up the environment</h1>
+    <div className='flex justify-center mt-32'>
+      <h1 className='w-[360px] h-[379px] bg-[#0B2434] flex justify-center items-center'>
+        <div className='w-[320px] h-[320px] bg-[#F5F5F5] rounded-md'></div>
+      </h1>
     </div>
   );
 }
