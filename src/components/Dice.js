d2fd0adebd734508699853c3525346bf28fcef08
John U <arjorb@gmail.com>
2023-03-02 00:28:25 +0200

    fixing the font weight

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Dice = props => {
   return (
     <div
-      className={`py-3 px-5 mt-2 cursor-pointer rounded-md font-bold ${
+      className={`py-3 px-5 mt-2 cursor-pointer rounded-md font-semibold ${
         props.isHeld ? 'bg-yellow-500 text-white' : 'bg-white'
       }`}
       onClick={props.handleHold}
