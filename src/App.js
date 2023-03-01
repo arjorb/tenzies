0ebc73b9820c02a56d2ed3144808cb2f5692f890
John U <arjorb@gmail.com>
2023-03-01 14:25:25 +0200

    done working on the function that generate the new dice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import Dice from './components/Dice';
-function App() {
+const App = () => {
+  const generateNewDice = () => {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      const randomNumber = Math.ceil(Math.random() * 6);
+      newDice.push(randomNumber);
+    }
+    console.log(newDice);
+    return newDice;
+  };
+
   return (
     <div className='flex justify-center mt-32'>
-      <h1 className='w-[360px] h-[379px] bg-[#0B2434] flex justify-center items-center'>
-        <div className='w-[320px] h-[320px] bg-[#F5F5F5] rounded-md'>
+      <h1 className='w-[380px] h-[399px] bg-[#0B2434] rounded-md flex justify-center items-center'>
+        <div className='w-[340px] h-[340px] bg-[#F5F5F5] rounded-md'>
           <h1 className='text-center mt-10 font-bold text-2xl text-[#2B283A]'>Tenzies</h1>
           <p className='text-center mt-2 font-light text-sm  w-10/12 mx-auto text-[#4A4E74]'>
             Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
           </p>
-          <div className='flex flex-wrap gap-3 w-11/12 mx-auto '>
-            <Dice />
-            <Dice />
-            <Dice />
-            <Dice />
-            <Dice />
-            <Dice />
-            <Dice />
-            <Dice />
-            <Dice />
-            <Dice />
+          <div className='flex flex-wrap gap-3 justify-center mx-auto '>
+            <Dice value='1' />
+            <Dice value='2' />
+            <Dice value='3' />
+            <Dice value='4' />
+            <Dice value='5' />
+            <Dice value='6' />
+            <Dice value='7' />
+            <Dice value='8' />
+            <Dice value='9' />
+            <Dice value='10' />
           </div>
           <div className='flex justify-center mt-3'>
             <button className='bg-blue-400 py-2 px-7 rounded-md text-white font-bold'>Roll</button>
@@ -27,6 +37,6 @@ function App() {
       </h1>
     </div>
   );
-}
+};
 
 export default App;
